bb21da71c27a58c61336d1a9b21b736fc77bf77e
MattCordeiro <matthew3cord@gmail.com>
2014-06-06 13:32:55 -0700

    added media queries phone

--- themes/digital/header.php
+++ themes/digital/header.php
@@ -31,7 +31,6 @@
 		<nav id="site-navigation" class="main-navigation header-navigation" role="navigation">
             <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'digital' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
         </div>
