989a94fa5b9909fd6d02f633d6249713ee6dbb47
MattCordeiro <matthew3cord@gmail.com>
2014-08-28 10:39:13 -0700

    Added files from git root

---
+++ plugins/swiftype-search/assets/install_te.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = '//s.swiftypecdn.com/te.js';
+var entry = document.getElementsByTagName('script')[0];
+entry.parentNode.insertBefore(script, entry);
